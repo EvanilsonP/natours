e5f9e86de079427049bb38ae997212cebdd1cbe3
EvanilsonP <evanilsonp99@gmail.com>
2023-10-20 11:28:37 -0300

    Setting up Express and Basic Routing

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.status(404).json({ message: "Hello from the server!"});
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint.');
+});
+
+app.listen(3000, () => console.log(`Running on PORT ${PORT}`));
