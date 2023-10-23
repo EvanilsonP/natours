a9d8c60e34d48de74c3ab590835a984d37a5aed3
Evanilson Pereira <pevanilson99@gmail.com>
2023-10-23 11:48:58 -0300

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
+    
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
 
@@ -30,6 +30,26 @@ app.post('/api/v1/tours', (req, res) => {
                 tour: newTour
             }
         });
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    // Converting a string into a number
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if(id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    };
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tour
+        }
     });
 });
 
