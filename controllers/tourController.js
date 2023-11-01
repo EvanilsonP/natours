dc73051b6e92dece0814d516bc3f6601f4ecc525
Evanilson Pereira <pevanilson99@gmail.com>
2023-11-01 11:15:12 -0300

    Modeling the tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,7 +57,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      data: 'Invalid data sent'
+      data: err
     });
     console.log(err);
   }
