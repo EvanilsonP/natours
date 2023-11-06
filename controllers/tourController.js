d7a2191cc9430f1c598f4237006476e47ba82502
Evanilson Pereira <pevanilson99@gmail.com>
2023-11-06 10:04:19 -0300

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,19 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
 
   try {
-    const tours = await Tour.find();
+    // Build query
+    const queryObj = {...req.query};
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // Filtering data
+    const query = Tour.find(queryObj);
+
+    // Filtering data
+    // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+    // Execute the query
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
