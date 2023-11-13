a53604d9d17200c668bc91dd0b7859c80ad0a847
Evanilson Pereira <pevanilson99@gmail.com>
2023-11-13 09:45:29 -0300

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -28,4 +25,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app;
+// Unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    'message': `Can't find ${req.originalUrl} on this server.`
+  });
+});
+
+module.exports = app;0
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-const mongoose = require('mongoose');
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-}).then(con => {
-  // console.log(con.connections);
-  console.log('DB connection successful!');
-})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
